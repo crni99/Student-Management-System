d9be527a7d8a517648e11a939f3e79da3a4dd9c6
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2022-03-30 13:16:48 +0200

    .

--- student-ms/src/main/resources/db/data.sql
+++ student-ms/src/main/resources/db/data.sql
@@ -1,17 +1,29 @@
 INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
-					 (1, 'Ognjen', 'Andjelic', '1999-12-01', 'andjelicb.ognjen@gmail.com', 183, true);
+					 (1, 'Ashley', 'Miller', '1999-12-01', 'ashleymiller@gmail.com', 183, true);
 
 INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
-				 	 (2, 'Nikola', 'Petrovic', '1999-08-25', 'nikola@gmail.com', 169, false);
+				 	 (2, 'Julia', 'Lopez', '1999-08-25', 'julialopez@gmail.com', 333, false);
 				 	 
 INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
-					 (3, 'Matija', 'Zoric', '2002-03-15', 'matijazoric@gmail.com', 333, true);
+					 (3, 'Eric', 'Smith', '2002-03-15', 'ericsmith@gmail.com', 3, true);
 
 INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
-				 	 (4, 'Jovana', 'Maricic', '2001-10-12', 'jmaricic@gmail.com', 3, false);
+				 	 (4, 'Joel', 'Hernandez', '2001-10-12', 'jhernandez@gmail.com', 169, false);
 				 	 
 INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
-				 	 (5, 'Davor', 'Cajic', '1999-06-12', 'davorcajic@gmail.com', 236, false);
+				 	 (5, 'Chelsey', 'Woods', '1999-06-12', 'woordsc@gmail.com', 236, false);
 				 	 
 INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
-				 	 (6, 'Igor', 'Mijailovic', '1999-08-16', 'igormijailovic@gmail.com', 234, false);
+				 	 (6, 'Steven', 'Ray', '1996-05-18', 'steven.ray@gmail.com', 862, false);
+				 	 
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
+				 	 (7, 'Cheyenne', 'Sherman', '1997-04-06', 'cheyenne.sherman@gmail.com', 56, true);
+				 	 
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
+				 	 (8, 'James', 'Callahan', '2000-03-26', 'james.c@gmail.com', 345, true);
+				 	 
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
+				 	 (9, 'Jason', 'Saunders', '2001-08-01', 'saunders.j@gmail.com', 987, true);
+				 	 
+INSERT INTO STUDENTS (id, first_name, last_name, date_of_birth, email, index_number, is_on_budget) VALUES 
+				 	 (10, 'Aaron', 'Gordon', '2002-03-17', 'aa.gordon@gmail.com', 56, false);
